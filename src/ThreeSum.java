e3f7f44a8c9f5706f459b31f96bad0d201e71b28
Tiangang Liu <liutg@hotmail.com>
2012-10-02 23:35:50 -0700

    3sum first solution

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -1,4 +1,48 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
 public class ThreeSum {
+    
+    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ArrayList<ArrayList<Integer>> out = new ArrayList<ArrayList<Integer>>();
+        
+        Arrays.sort(num);
+        int length = num.length;
+        
+        for (int i = 0; i < length; i++) {
+            for (int j = i+1; j < length; j++) {
+                int rest = 0 - num[i] - num[j];
+                int k = -1;
+                int lo = j + 1; 
+                int high = length -1;
+                while (lo <= high) {
+                    int mid = lo + (high - lo)/2;
+                    if (num[mid] < rest)
+                        lo = mid + 1;
+                    else if (num[mid] > rest)
+                        high = mid -1;
+                    else {
+                        k = mid;
+                        break;
+                    }
+                        
+                }
+                
+                if (k != -1) {
+                    ArrayList<Integer> item = new ArrayList<Integer>();
+                    item.add(num[i]);
+                    item.add(num[j]);
+                    item.add(num[k]);
+                    out.add(item);
+                }
+                
+            }
+            
+            
+        }
+        
+        return out;
+    }
 
 }
