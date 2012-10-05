c8150ecc3538c7e1b923e9312a9a9cdcbafdab99
Tiangang Liu <liutg@hotmail.com>
2012-10-04 23:07:59 -0700

    3SumClosest v3
    final working version

--- src/ThreeSumClosest.java
+++ src/ThreeSumClosest.java
@@ -5,17 +5,22 @@ public class ThreeSumClosest {
 
     public int threeSumClosest(int[] num, int target) {
         
-        
-        Arrays.sort(num);
         int length = num.length;
-        int diff = target - num[0] - num[1] - num[2];
+        
+        if (length == 3)
+            return num[0] + num[1] + num[2];
+
+        Arrays.sort(num);
+        int best = num[0] + num[1] + num[2];
+        
         for (int i = 0; i < length; i++) {
-            for (int j = i+1; j < length; j++) {
+            for (int j = i+1; j < length - 1; j++) {
                 int rest = target - num[i] - num[j];
                 
                 // Binary search 
 
                 int lo = j + 1;
+                
                 int high = length -1;
                 while (lo <= high) {
                     int mid = lo + (high-lo)/2;
@@ -27,20 +32,23 @@ public class ThreeSumClosest {
                         return target;
                         
                 }
-                if (lo > length - 1)
-                    lo = length - 1;
+
                 int next;
-                if (Math.abs(rest - num[lo]) > Math.abs(rest -num[lo-1]))
-                    next = num[lo-1] + num[i]+ num[j];
-                else
-                    next = num[lo] + num[i]+ num[j];
                 
-                if (Math.abs(target - next) < Math.abs(diff))
-                    diff = target - next;
+                if (high < j + 1)
+                    next = num[high+1] + num[i]+ num[j];
+                else if (high < length - 1 
+                        && Math.abs(rest -num[high+1]) < Math.abs(rest -num[high]))
+                    next = num[high+1] + num[i]+ num[j];
+                else
+                    next = num[high] + num[i]+ num[j];
+                
+                if (Math.abs(target - next) < Math.abs(target - best))
+                    best = next;
                 
             }
         }
-        return target - diff;
+        return best;
 
         
     }
@@ -49,10 +57,12 @@ public class ThreeSumClosest {
         int[] a1 = {0, 0, 0};
         int[] a2 = {0, 1, 2};
         int[] a3 = {-1, 2, 1, -4};
+        int[] a4 = {0, 2, 1, -3};
         ThreeSumClosest c = new ThreeSumClosest(); 
         System.out.println(c.threeSumClosest(a1, 1));   //expect 0
         System.out.println(c.threeSumClosest(a2, 0));   //expect 3
         System.out.println(c.threeSumClosest(a3, 1));   //expect 2
+        System.out.println(c.threeSumClosest(a4, 1));   //expect 0
         
     }
 }
