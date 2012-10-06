f3b4adaffdc9ac5277efe826c85ff0b75bacac0a
Tiangang Liu <liutg@hotmail.com>
2012-10-05 17:16:39 -0700

    4sum first version

---
+++ src/FourSum.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class FourSum {
+    public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
+
+        
+        ArrayList<ArrayList<Integer>> out = new ArrayList<ArrayList<Integer>>();
+        int length = num.length;
+        if (length < 4)
+            return out;
+        
+        Arrays.sort(num);
+        
+        
+        return out;
+        
+    }
+}
+
+
