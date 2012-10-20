623d8aeafcc351b20a997fd030500b0f859ebeee
Tiangang Liu <liutg@hotmail.com>
2012-10-19 17:41:56 -0700

    anagrams initial

---
+++ src/Anagrams.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class Anagrams {
+    public ArrayList<String> anagrams(String[] strs) {
+        ArrayList<String> out = new ArrayList<String>();
+        
+        for (String s:strs) {
+            
+        }
+        return out;
+        
+    }
+}
