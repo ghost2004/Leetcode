29728c33bc0b610945f3dc0788890e895dc6bcd2
Tiangang Liu <liutg@hotmail.com>
2012-10-21 23:15:30 -0700

    levelOrderBottom Ugly implement

--- src/LevelTravel.java
+++ src/LevelTravel.java
@@ -59,4 +59,49 @@ public class LevelTravel {
         return out;
 
     }
+    
+    public ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) {
+        ArrayList<ArrayList<Integer>> out = new ArrayList<ArrayList<Integer>>();
+                
+        if (root == null)
+            return out;
+        ValPack p =  new ValPack();
+        p.node = root;
+        p.level = 0;
+        LinkedList<ValPack> nodeList = new LinkedList<ValPack>();
+        Stack<ArrayList<Integer>> stack = new Stack<ArrayList<Integer>>();
+        ArrayList<Integer> item = new ArrayList<Integer>();
+        nodeList.addLast(p);
+        int curLevel = 0;
+        while (nodeList.size() != 0) {
+            p = nodeList.removeFirst();
+            if (p.level != curLevel) {
+                stack.push(item);
+                item = new ArrayList<Integer>();
+                curLevel = p.level;
+            }
+            item.add(p.node.val);
+            
+            if (p.node.left != null) {
+                ValPack v = new ValPack();
+                v.level = curLevel + 1;
+                v.node = p.node.left;
+                nodeList.addLast(v); 
+            }
+            if (p.node.right != null) {
+                ValPack v = new ValPack();
+                v.level = curLevel + 1;
+                v.node = p.node.right;
+                nodeList.addLast(v); 
+            }
+                
+        }
+        out.add(item);
+        while (!stack.empty()) {
+            out.add(stack.pop());
+        }
+        
+        return out;
+        
+    }
 }
