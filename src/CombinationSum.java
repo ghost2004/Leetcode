139b29d109a95f5803f2b77a88440af9a33639ed
Tiangang Liu <liutg@hotmail.com>
2012-10-30 18:33:24 -0700

    CombinationSum initial version

---
+++ src/CombinationSum.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class CombinationSum {
+    public ArrayList<ArrayList<Integer>> 
+    combinationSum(int[] candidates, int target) {
+        ArrayList<ArrayList<Integer>>  out = new ArrayList<ArrayList<Integer>>();
+        return out;
+    }
+
+}
