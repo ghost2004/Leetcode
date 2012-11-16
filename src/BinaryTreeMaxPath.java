3e168662e3aef5d4ef8f4933be6afa0327cb4ae6
Tiangang Liu <liutg@hotmail.com>
2012-11-16 00:02:10 -0800

    Binary Tree Maximum Path Sum Initial version

---
+++ src/BinaryTreeMaxPath.java
@@ -0,0 +1,35 @@
+/*
+ * Given a binary tree, find the maximum path sum.
+
+The path may start and end at any node in the tree.
+
+For example:
+Given the below binary tree,
+
+       1
+      / \
+     2   3
+Return 6.
+ * 
+ */
+public class BinaryTreeMaxPath {
+    public class TreeNode {
+        private int val;
+        private TreeNode left;
+        private TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    
+    public class TreeVal {
+        private int self;
+        private int export;
+    }
+    
+    
+    
+    public int maxPathSum(TreeNode root) {
+
+        return 0;
+        
+    }
+}
