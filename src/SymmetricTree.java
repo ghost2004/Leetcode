0d67ef3e735ec2d0ce3e29dfc0e215e066a6f25d
Tiangang Liu <liutg@hotmail.com>
2013-08-07 23:55:35 -0700

    symmtric tree v2

--- src/SymmetricTree.java
+++ src/SymmetricTree.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 /*
  * Given a binary tree, check whether it is a mirror of itself
  *  (ie, symmetric around its center).
@@ -24,6 +25,7 @@ Bonus points if you could solve it both recursively and iteratively.
  */
 public class SymmetricTree {
     
+    // Inorder travseral solution
     public void inOrder(TreeNode node, ArrayList<Integer> list)
     {
         if (node == null)
@@ -33,7 +35,7 @@ public class SymmetricTree {
         inOrder(node.right, list);
     }
     
-    public boolean isSymmetric(TreeNode root) {
+    public boolean isSymmetricV1(TreeNode root) {
         if (root == null)
             return true;
         ArrayList<Integer> list = new ArrayList<Integer>();
@@ -48,5 +50,60 @@ public class SymmetricTree {
         
         return true;    
     }
+    
+    // recursively solution
+    public boolean checkSym(TreeNode left, TreeNode right)
+    {
+        if (left == null && right == null)
+            return true;
+        if (left == null || right == null)
+            return false;
+        if (left.val != right.val)
+            return false;
+        return (checkSym(left.left, right.right)
+                && checkSym(left.right, right.left));
+    }
+    public boolean isSymmetricV2(TreeNode root) {
+        if (root == null)
+            return true;
+        return checkSym(root.left, root.right);
+    }
+    
+    // iteratively solution
+    
+    public boolean isSymmetricV3(TreeNode root) {
+        if (root == null)
+            return true;
+        LinkedList<TreeNode> left = new LinkedList<TreeNode>();
+        LinkedList<TreeNode> right = new LinkedList<TreeNode>();
+        
+        left.addLast(root.left);
+        right.addLast(root.right);
+        
+        while (!left.isEmpty() && !right.isEmpty()) {
+            TreeNode nl = left.removeFirst();
+            TreeNode nr = right.removeFirst();
+            
+            if (nl == null && nr == null)
+                continue;
+            if (nl == null || nr == null)
+                return false;
+            if (nl.val != nr.val)
+                return false;
+            left.addLast(nl.left);
+            left.addLast(nl.right);
+            right.addLast(nr.right);
+            right.addLast(nr.left);
+        }
+        
+        if (left.isEmpty() && right.isEmpty())
+            return true;
+        
+        return false;
+        
+              
+
+    }
+    
 
 }
