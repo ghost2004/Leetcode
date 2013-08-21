80116523dd59c1f758a1c43702f0f5963792f73b
Tiangang Liu <liutg@hotmail.com>
2013-08-20 23:55:45 -0700

    solution 2 for recover BST
    Morris traversal

--- src/RecoverBST.java
+++ src/RecoverBST.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+
 /*
  * Two elements of a binary search tree (BST)
  *  are swapped by mistake.
@@ -12,18 +12,20 @@ A solution using O(n) space is pretty straight forward.
 public class RecoverBST {
     
     TreeNode[] nodes;
-    TreeNode prev;
+    TreeNode pre;
     int idx;
 
-    
+    // Solution with inorder traversal 
+    // Need O(lgn) stack space in average, 
+    // O(n) in the worst case
     public void searchMis(TreeNode cur)
     {
         if (cur == null)
             return;
         searchMis(cur.left);
-        if (prev != null && prev.val > cur.val) {
+        if (pre != null && pre.val > cur.val) {
             if (idx == 0) {
-                nodes[0] = prev;
+                nodes[0] = pre;
                 nodes[1] = cur;
                 idx = 1;
             } else {
@@ -32,7 +34,7 @@ public class RecoverBST {
             
                 
         }
-        prev = cur;
+        pre = cur;
         
         searchMis(cur.right);
     }
@@ -43,22 +45,82 @@ public class RecoverBST {
         nodes[0] = null;
         nodes[1] = null;
 
-        prev = null;
+        pre = null;
         searchMis(root);
         int tmp = nodes[0].val;
         nodes[0].val = nodes[1].val;
         nodes[1].val = tmp;
     }
     
+    // Morris Traversal solution
+    public void recoverTreeMorris(TreeNode root) {
+        TreeNode n1 = null;
+        TreeNode n2 = null;
+        TreeNode prev = null;
+        TreeNode parent = null;
+        TreeNode cur = root;
+        
+        while (cur != null) {
+            if (cur.left == null) {
+                // go to right sub tree when left is empty
+                if (parent != null && parent.val > cur.val) {
+                    if (n1 == null)
+                        n1 = parent;
+                    n2 = cur;
+                }
+                parent = cur;
+                cur = cur.right;
+            } else {
+                // find predecessor
+                prev = cur.left;
+                
+                while (prev.right != null && prev.right != cur)
+                    prev = prev.right;
+                
+                if (prev.right == null) {
+                    // set the right child of previous node 
+                    // to cur when it is null
+                    prev.right = cur;
+                    // move to its left child
+                    cur = cur.left;
+                    
+                    
+                } else {
+                    // prev.right == cur
+                    // remove the additional link
+                    prev.right = null;
+                    if (parent != null && parent.val > cur.val) {
+                        if (n1 == null)
+                            n1 = parent;
+                        n2 = cur;
+                    }
+                    parent = cur;
+                    cur = cur.right;
+
+                }
+                   
+                
+                
+            }
+        }
+        
+
+        int tmp = n1.val;
+        n1.val = n2.val;
+        n2.val = tmp;
+        
+    }
+    
+    
     public static void main(String[] args)
     {
         RecoverBST r = new RecoverBST();
-        TreeNode root = new TreeNode(3);
+        TreeNode root = new TreeNode(2);
         TreeNode left = new TreeNode(1);
-        TreeNode right = new TreeNode(2);
+        TreeNode right = new TreeNode(3);
         root.right = left;
         root.left = right;
-        r.recoverTree(root);
+        r.recoverTreeMorris(root);
         root.print();
     }
 }
