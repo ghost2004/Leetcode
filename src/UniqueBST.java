1584eb634eb1d21774fe9f47a2dd7ef4852f14b7
Tiangang Liu <liutg@hotmail.com>
2013-08-23 23:54:38 -0700

    Unique BST DP solution

--- src/UniqueBST.java
+++ src/UniqueBST.java
@@ -28,12 +28,37 @@ public class UniqueBST {
         
     }
     
+    public int numTrees2(int n)
+    {
+        if (n == 0 || n == 1)
+            return 1;
+        int[] num = new int[n+1];
+        num[0] = 1;
+        num[1] = 1;
+        num[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= i; j++)
+            {
+                int left = j - 1;
+                int right = i - j;
+                cnt += num[left]*num[right];
+                
+            }
+            
+            num[i] = cnt;
+        }
+        return num[n];
+        
+    }
+    
     public static void main(String[] args)
     {
         UniqueBST u = new UniqueBST();
         
         int[] a = {0, 1, 2, 3, 4, 5};
         for (int i:a)
-            System.out.println(i+"--"+u.numTrees(i));
+            System.out.println(i+"--"+u.numTrees2(i));
     }
 }
