29b00e89c2bf04eff5c40c529658d061f7bee836
Tiangang Liu <liutg@hotmail.com>
2013-09-02 17:02:29 -0700

    Interleaving string 2nd solution

--- src/InterleavingString.java
+++ src/InterleavingString.java
@@ -41,6 +41,48 @@ public class InterleavingString {
         return checkInterleave(s1.length()-1, s2.length()-1, s3.length()-1);
     }
     
+    public boolean isInterLeave2(String s1, String s2, String s3) {
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int len3 = s3.length();
+        
+        if (len1 + len2 != len3)
+            return false;
+        
+        boolean[] flags = new boolean[len2+1];
+        flags[len2] = true;
+        // set up the flag array
+        // flag is set to true if it is s2 the substring of s3 at tail
+        int i, j;
+        for (i = len2-1; i >= 0; i--) {
+            if (s3.charAt(len1+i) == s2.charAt(i) && flags[i+1])
+                flags[i] = true;
+            else
+                flags[i] = false;
+        }
+        // scan s1 from end to beginning 
+        for (i = len1-1; i >= 0; i--) {
+            // scan s2 from end to beginning
+            for (j = len2-1; j >= 0; j--) {
+                if (s3.charAt(i+j) == s1.charAt(i) && flags[j])
+                    flags[j] =  true;
+                else if (s3.charAt(i+j) == s2.charAt(j) && flags[j+1])
+                    flags[j] = true;
+                else
+                    flags[j] = false;
+            }
+            
+            if (flags[len2] && s3.charAt(i+len2) == s1.charAt(i))
+                flags[len2] = true;
+            else
+                flags[len2] = false;
+            
+        }
+        
+        return flags[0];
+        
+    }
+    
     public static void main(String[] args)
     {
         InterleavingString t = new InterleavingString();
