867b6fae06e562fdab7d1e53bb9dfd2ea98a0b91
Tiangang Liu <liutg@hotmail.com>
2013-09-08 23:46:17 -0700

    Maximal Rectangle 2nd solution

--- src/MaxRectangle.java
+++ src/MaxRectangle.java
@@ -2,8 +2,11 @@
  * Given a 2D binary matrix filled with 0's and 1's, find the
  *  largest rectangle containing all ones and return its area.
  */
+import java.util.Stack;
 public class MaxRectangle {
-    public int maxRectArea(char[][] matrix, int row, int col) {
+    
+    // Solution for  O(N^3)
+    private int maxRectArea(char[][] matrix, int row, int col) {
         int area = 1;
         int minWidth = Integer.MAX_VALUE;
         for (int i = row; i < matrix.length && matrix[i][col] == '1'; i++) {
@@ -36,4 +39,61 @@ public class MaxRectangle {
         return max;
         
     }
+    
+    private int maxHistogram(int[] ht) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int idx = 0;
+        int max = 0;
+        int size = ht.length;
+        while (idx < size) {
+            if (stack.isEmpty() || ht[idx] >= ht[stack.peek()]) {
+                stack.push(idx);
+                idx++;
+            } else {
+                int lastIdx = stack.pop();
+                int area;
+                if (stack.isEmpty())
+                    area = idx*ht[lastIdx];
+                else
+                    area = (idx-stack.peek()-1)*ht[lastIdx];
+                max = Math.max(max, area);
+            }
+        }
+        return max;
+    }
+    
+    // Solution for  O(N^2)
+    public int maximalRectangle2(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return 0;
+
+        int max = 0;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] height = new int[row][col+1];
+        
+        int i, j;
+        
+        for (i = 0; i < row; i++) {
+            for (j = 0; j < col; j++) {
+                if (matrix[i][j] == '0')
+                    height[i][j] = 0;
+                else {
+                    if (i == 0)
+                        height[i][j] = 1;
+                    else
+                        height[i][j] = height[i-1][j] + 1;
+                }
+                    
+            }
+            height[i][col] = 0;
+        }
+        
+        for (i = 0; i < row; i++) {
+            max = Math.max(max, maxHistogram(height[i]));
+        }
+        
+        return max;
+    }
+    
 }
