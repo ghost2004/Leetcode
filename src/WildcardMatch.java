9a3770f69508dbacc078ba8522f8b2d8355c74d8
Tiangang Liu <liutg@hotmail.com>
2013-09-24 23:35:15 -0700

    Wildcard Matching failed version

--- src/WildcardMatch.java
+++ src/WildcardMatch.java
@@ -18,14 +18,72 @@ isMatch("aab", "c*a*b") ¡ú false
  */
 public class WildcardMatch {
     public boolean isMatch(String s, String p) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        if (p == null || "".equals(p))
+        if (p == null)
             return true;
-        if (s == null || "".equals(s))
+        if (s == null)
             return false;
         
+       
+        int pIdx = 0;
+        int sIdx = 0;
+        int pLen = p.length();
+        int sLen = s.length();
+        boolean flag = false;
         
+        while (pIdx < pLen && sIdx < sLen) {
+            char key = p.charAt(pIdx);
+            if (key == '?') {
+                sIdx++;
+            } else if (key == '*') {
+                flag = true;
+            } else {
+                if (flag) {
+                    flag = false;
+                    while (sIdx < sLen-1 && s.charAt(sIdx) != key)
+                        sIdx++;
+                    if (s.charAt(sIdx) != key)
+                        return false;
+                    
+                        
+                } else {
+                    if (s.charAt(sIdx) != key)
+                        return false;
+                    sIdx++;
+                }
+            }
+            
+            pIdx++;
+        }
         
+        if (flag && pIdx == pLen)
+            return true;
+        
+        while (pIdx < pLen) {
+            if (p.charAt(pIdx) != '*')
+                return false;
+            pIdx++;
+        }
+        
+        if (sIdx != sLen)
+            return false;
+        
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        WildcardMatch w = new WildcardMatch();
+
+        System.out.println(w.isMatch("aa", "a"));
+        System.out.println(w.isMatch("aa", "aa"));
+        System.out.println(w.isMatch("aaa", "aa"));
+        System.out.println(w.isMatch("aa", "*"));
+        System.out.println(w.isMatch("aa", "a*"));
+        System.out.println(w.isMatch("ab", "?*"));
+        System.out.println(w.isMatch("aab", "c*a*b"));
+        System.out.println(w.isMatch("b", "*a*"));
+        System.out.println(w.isMatch("", "*"));
+        System.out.println(w.isMatch("a", ""));
+        System.out.println(w.isMatch("b", "*?*?"));
+        System.out.println(w.isMatch("a", "*a"));
     }
 }
